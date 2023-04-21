04f5e93eb41afc962b58a725c9d63dde2c64518c
Eduardo Tadeu <eduardo.tsilva@hotmail.com>
2023-04-21 19:41:34 -0400

    Criando projeto angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Alurapic';
+
+  description = "Leão";
+  url = "https://img.freepik.com/fotos-gratis/imagem-aproximada-da-cabeca-de-um-lindo-leao_181624-35855.jpg?w=900&t=st=1682117766~exp=1682118366~hmac=7980f7296be5e5e4aaaa4a346365a2bcf4c27d615bd0268f825ab62f5d69e9ad";
+
 }
