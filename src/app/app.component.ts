c1b0bfaecba0fbad347a25fcc78e8be3958923a7
Eduardo Tadeu <eduardo.tsilva@hotmail.com>
2023-04-22 10:59:58 -0400

    Organizando dependencia do modulo photos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PhotoService } from './photos/photo/photo.service';
 import { Photo } from './photos/photo/photo';
 
@@ -8,18 +8,16 @@ import { Photo } from './photos/photo/photo';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   photos: Photo[] = [];
 
-  constructor(photoService: PhotoService) {
+  constructor(private photoService: PhotoService) { }
 
-    photoService
+  ngOnInit(): void {
+    this.photoService
       .listFromUser('flavio')
       .subscribe(photos => this.photos = photos);
-
   }
-
-
 
 }
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,10 +1,12 @@
-import { NgModel } from "@angular/forms";
+
 import { PhotoComponent } from "./photo/photo.component";
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [PhotoComponent],
-  exports: [PhotoComponent]
+  exports: [PhotoComponent],
+  imports: [HttpClientModule]
 })
 export class PhotosModule {
 
