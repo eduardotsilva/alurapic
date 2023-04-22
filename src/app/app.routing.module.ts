d96a7571987e7fd537fead4b49e480732146c659
Eduardo Tadeu <eduardo.tsilva@hotmail.com>
2023-04-22 18:11:43 -0400

    criado modulo quando a rota nao existe

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from "@angular/core";
 import { PhotoListComponent } from "./photos/photo-list/photo-list.component";
 import { PhotoFormComponent } from "./photos/photo-form/photo-form.component";
-import { Router, RouterModule, Routes } from '@angular/router';
+import { Router, RouterModule, Routes } from "@angular/router";
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 const routes: Routes = [
   { path: "user/flavio", component: PhotoListComponent },
   { path: "p/add", component: PhotoFormComponent },
+  { path: "**", component: NotFoundComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes) ],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
