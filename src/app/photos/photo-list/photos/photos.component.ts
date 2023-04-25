5b37b987214cf403d38de534b1a234da16ecc8bc
Eduardo Tadeu <eduardo.tsilva@hotmail.com>
2023-04-25 19:54:45 -0400

    Trabalhando com onChanges, SimpleChanges

--- src/app/photos/photo-list/photos/photos.component.ts
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -1,19 +1,36 @@
-import { Photo } from './../../photo/photo';
-import { Component, Input, OnInit } from '@angular/core';
+import { Photo } from "./../../photo/photo";
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from "@angular/core";
 
 @Component({
-  selector: 'app-photos',
-  templateUrl: './photos.component.html',
-  styleUrls: ['./photos.component.css']
+  selector: "app-photos",
+  templateUrl: "./photos.component.html",
+  styleUrls: ["./photos.component.css"],
 })
-export class PhotosComponent implements OnInit {
+export class PhotosComponent implements OnChanges {
+  @Input() photos: Photo[] = [];
+  rows: any[] = [];
 
+  constructor() {}
 
-@Input() photos: Photo[]=[];
-
-  constructor() { }
-
-  ngOnInit() {
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.photos) {
+      this.rows = this.grupoDeColunas(this.photos);
+    }
   }
 
+  grupoDeColunas(photos: Photo[]) {
+    const newRows = [];
+
+    for (let index = 0; index < photos.length; index += 3) {
+      newRows.push(photos.slice(index, index + 3));
+    }
+
+    return newRows;
+  }
 }
