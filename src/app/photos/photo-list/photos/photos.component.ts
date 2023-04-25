10de04ab9729bfda182c1d448f3444a726fddec7
Eduardo Tadeu <eduardo.tsilva@hotmail.com>
2023-04-25 19:27:14 -0400

    correção column bootstrap

---
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -0,0 +1,19 @@
+import { Photo } from './../../photo/photo';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-photos',
+  templateUrl: './photos.component.html',
+  styleUrls: ['./photos.component.css']
+})
+export class PhotosComponent implements OnInit {
+
+
+@Input() photos: Photo[]=[];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
